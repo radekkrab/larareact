d99e4354fa63b3582d6da959ecc2bc0f09d87e7b
radekkrab <radxxx7@mail.ru>
2024-03-11 15:52:43 +0500

    11031552

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -9,11 +9,19 @@ export default function Dashboard({ auth }) {
         >
             <Head title="Пополнить банковской картой" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
+            <div className="py-10">
+                <div className="max-w-[550px] mx-auto bg-white overflow-hidden shadow-sm sm:rounded-lg p-[40px]">
+                    <div className="text-gray-900 font-semibold text-[24px] mb-3">Пополнить банковской картой</div>
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="price1" className="form-label text-[12px] text-gray-700">УКАЖИТЕ СУММУ</label><br></br>
+                            <input type="number" className="form-control rounded-l-lg w-[144px] h-[52px] bg-[#FAFAFC] border-gray-200 mt-1" id="price1"
+                                   placeholder="0000.00  $"/>
+                            <input type="number" className="form-control rounded-r-lg w-[144px]  h-[52px] bg-[#FAFAFC] border-l-0 border-gray-200" id="price1"
+                                   placeholder="0000.00  Р"/>
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </AuthenticatedLayout>
